728275ae1822d9561212b850be9ff44cfd76d248
Dobby233Liu <15816141883@163.com>
2019-01-21 11:10:10 +0800

    Explain others

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rm -rf busybox* isoimage kernel* linux* *.iso syslinux*
 
+# all thing it do: 移除busybox，isoimage，内核，syslinux相关文件夹和全部iso
+rm -rf busybox* isoimage kernel* linux* *.iso syslinux*
--- qemu64.sh
+++ qemu64.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-qemu-system-x86_64 -m 128M -cdrom minimal_linux_live.iso -boot d -vga std
 
+# 运行qemu，vga模式，内存128m，cdrom为minimal_linux_live.iso
+qemu-system-x86_64 -m 128M -cdrom minimal_linux_live.iso -boot d -vga std
